7699dc43a29053e644ffc6f4da7fffd64b2fb85b
EetuH <eetuh25@gmail.com>
2025-02-17 15:36:57 +0200

    some initialization (#7)

---
+++ Program.cs
@@ -0,0 +1,15 @@
+namespace Budgetinator_2000;
+
+static class Program
+{
+    /// <summary>
+    ///  The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
+    {
+ 
+        ApplicationConfiguration.Initialize();
+        Application.Run(new BudgetinatorWindow());
+    }    
+}
---
+++ Views/BudgetinatorWindow.cs
@@ -0,0 +1,9 @@
+namespace Budgetinator_2000;
+
+public partial class BudgetinatorWindow : Form
+{
+    public BudgetinatorWindow()
+    {
+        InitializeComponent();
+    }
+}
